1fae7df03b643b4428e5d08769b971feeb0374b5
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-29 02:32:03 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,14 @@
+class Show < ActiveRecord::Base
+  def Show::highest_rating
+    Show.maximum("rating")
+  end
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
+  end
+  def Show::lowest_rating
+    Show.minimum("rating")
+  end
+  def Show::least_popular_show
+    Show.maximum("rating")
+  end
+end
